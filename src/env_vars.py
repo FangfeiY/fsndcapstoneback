56cb576605c9ca73780767705b4ff8ba05c8083d
Fangfei Yang <fey0131@hotmail.com>
2022-03-13 23:52:30 -0400

    Coorect env var name in script loading it.

--- src/env_vars.py
+++ src/env_vars.py
@@ -8,4 +8,4 @@ AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 AUTH0_API_AUDIENCE = os.environ.get('AUTH0_API_AUDIENCE')
 AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID')
 AUTH0_CLIENT_SEC = os.environ.get('AUTH0_CLIENT_SEC')
-METEOR_TESTING = True if os.environ.get('TESTING').lower() == 'true' else False
+METEOR_TESTING = True if os.environ.get('METEOR_TESTING').lower() == 'true' else False
